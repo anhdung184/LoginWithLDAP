ec631da30f9bdaac2e4c96764d225dee6460c30d
anhdung184 <Anhdungk58t1@gmail.com>
2023-10-11 14:59:57 +0700

    first commit

---
+++ src/main/java/com/example/LoginWithLDAP/LoginWithLdapApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoginWithLDAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithLdapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LoginWithLDAP/LoginWithLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoginWithLDAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
