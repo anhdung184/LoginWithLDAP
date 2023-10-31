68d7aa79060f6a7548b86d266dc527ceb53e3f73
anhdung184 <Anhdungk58t1@gmail.com>
2023-10-31 14:00:28 +0700

    update 31/10

--- src/main/java/com/example/LoginWithLDAP/Security/WebSecurityConfig.java
+++ src/main/java/com/example/LoginWithLDAP/Security/WebSecurityConfig.java
@@ -5,23 +5,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.security.Security;
-
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
                     .anyRequest().fullyAuthenticated()
                     .and()
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/")
-                        .permitAll());
+                .formLogin();
+
         return http.build();
     }
 
